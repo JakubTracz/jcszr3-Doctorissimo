947ac983c7c934b09bff4bde90f6b14ca9fc85cf
Jakub Tracz <jakub.tracz.1994@o2.pl>
2021-08-29 17:42:00 +0200

    Hide row JS

--- Doctorissimo/wwwroot/js/site.js
+++ Doctorissimo/wwwroot/js/site.js
@@ -1,17 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Basic example
-$(document).ready(function () {
-    $('#appointmentsTable').DataTable({
-        "paging": false
-    });
-    $('.dataTables_length').addClass('bs-select');
-});
-// Basic example
-$(document).ready(function () {
-    $('#appointmentsTable').DataTable({
-        "pagingType": "simple" // "simple" option for 'Previous' and 'Next' buttons only
-    });
-    $('.dataTables_length').addClass('bs-select');
-});
+function ToggleRowDisplay() {
+    const nextRow = (this).parentNode.row.nextElementSibling();
+    alert(nextRow.innerHTML);
+}
