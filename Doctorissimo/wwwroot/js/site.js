cb398aeb4101d1a17d41a8e938c16acab2136d20
Jakub Tracz <jakub.tracz.1994@o2.pl>
2021-09-05 14:41:19 +0200

    ToogleRowDisplay function fix

--- Doctorissimo/wwwroot/js/site.js
+++ Doctorissimo/wwwroot/js/site.js
@@ -1,8 +1,9 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-function ToggleRowDisplay() {
-    const row = $(this).closest("tr");
-    const nextRow = row.next();
-    nextRow.addClass("test");
-}
+//function ToggleRowDisplay() {
+//    const row = $(this).closest("tr");
+//    alert(row.innerHTML);
+//    const nextRow = row.next();
+//    nextRow.addClass("test");
+//}
